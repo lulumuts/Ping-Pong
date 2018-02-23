369901e3229057f89d6f53dc3fdddf1d30b3f439
Lulu <lulumutuli24@gmail.com>
2018-02-23 16:45:07 +0300

    initial backend javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
+//back-end work
+function myFunction() {
+  var x, text;
+
+  x = document.getElementById('enter').value;
+
+  if (isNan(x) || x === 3) {
+    text = "ping";
+  } else {
+    text = "pong";
+  }
+
+//front-end response
 $(document).ready(function() {
   $("button.PING").click(function(){
-    var response = $("ul").getElementById('<li>enter</li>');
+    var response = $("ul").prepend("<li>text</li>");
+
     });
   });
 
-// function myFunction() {
-//   var x, text;
-//
-//   x = document.getElementById('enter').value;
-//
-//   if (isNan(x) || x < 1 || x > 10) {
-//     text = "no";
-//   } else {
-//     text = "OK";
-//   }
+
 //
 //   document.getElementById('response').innerHTML = text;
 //   alert("no");
