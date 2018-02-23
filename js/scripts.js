7c2ef1d62634b4883299805fd2963c4635a02b88
Lulu <lulumutuli24@gmail.com>
2018-02-23 16:31:52 +0300

    initial javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-  $("button#submit").click(function() {
-    $("ul.response").append("<li>#submit</li>");
-    event.preventDefault();
+  $("button.PING").click(function(){
+    var response = $("ul").getElementById('<li>enter</li>');
+    });
   });
-});
+
+// function myFunction() {
+//   var x, text;
+//
+//   x = document.getElementById('enter').value;
+//
+//   if (isNan(x) || x < 1 || x > 10) {
+//     text = "no";
+//   } else {
+//     text = "OK";
+//   }
+//
+//   document.getElementById('response').innerHTML = text;
+//   alert("no");
